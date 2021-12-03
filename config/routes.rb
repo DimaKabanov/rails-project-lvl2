64e21955d9f117597e8dabbe4beb1387c80e6a5d
Dmitry Kabanov <dkabanov6@gmail.com>
2021-12-03 06:25:26 +0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope '(:locale)', locale: /en|ru/ do
-    root 'posts#index'
-    devise_for :users
-    resources :posts, only: %i[index new create show] do
-      scope module: :posts do
-        resources :comments, only: %i[create]
-        resources :likes, only: %i[create destroy]
-      end
+  root 'posts#index'
+  devise_for :users
+  resources :posts, only: %i[index new create show] do
+    scope module: :posts do
+      resources :comments, only: :create
+      resources :likes, only: %i[create destroy]
     end
   end
 end
