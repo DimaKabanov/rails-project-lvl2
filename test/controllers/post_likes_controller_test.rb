2ec20eb3268b5b094d319dd7b4c17e87928fa6ff
Dmitry Kabanov <dkabanov6@gmail.com>
2021-12-18 06:22:35 +0500

    updated likes tests

--- test/controllers/post_likes_controller_test.rb
+++ test/controllers/post_likes_controller_test.rb
@@ -6,21 +6,23 @@ class PostLikesControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
-    sign_in users(:one)
+    @post = posts(:one)
+    @user = users(:one)
+    sign_in @user
   end
 
   test 'should create like' do
-    post = posts(:two)
-    assert_difference('post.likes.count') do
-      post post_likes_path(post)
-    end
+    post post_likes_path(@post)
+    like = @post.likes.find_by(user_id: @user.id)
+
+    assert { like }
   end
 
   test 'should destroy like' do
-    post = posts(:one)
     like = post_likes(:two)
-    assert_difference('post.likes.count', -1) do
-      delete post_like_path(post, like)
-    end
+
+    delete post_like_path(@post, like)
+
+    assert { !@post.likes.find_by(id: like.id) }
   end
 end
